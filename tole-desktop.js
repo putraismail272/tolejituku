ab4a4782a36603530c0f439961fc9a9d4029c2f1
putraismail272 <124043702+putraismail272@users.noreply.github.com>
2023-06-01 20:11:56 +0700

    Create tole-desktop.js

---
+++ tole-desktop.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function(){
+$('<li><a href="https://prediksitogeljitu.org/?page=prediksi-togel" target="_blank"><i class="fa fa-star"></i>  <span class="blink">PREDIKSI TOGEL</span></a></li>').insertAfter(".nav>li:last-child");
+
+$('<a title="Daftar Slot togel" target="_blank" href="#"><img src="https://i.postimg.cc/7LPLbDFy/banner-gif-tole-jitu.gif"alt="tolejitu" style="width:100%; margin-bottom: 5px; margin-top: 8px;" /></a>').insertAfter("#slider");
+
+  },false);
+
+$('#bank>.container>.row>.col-md-3').remove();
+$('.bankscroll').next('div').remove();
+$('#latest-results').removeClass('margin-top-15');
