d3123ebe277880e237dfcdbd961c21bfae723675
putraismail272 <124043702+putraismail272@users.noreply.github.com>
2023-06-08 19:10:09 +0700

    Update and rename tole-mobile-4.js to tole-mobile-5.js

--- tole-mobile-5.js
+++ tole-mobile-5.js
@@ -45,10 +45,10 @@ $('<div style="text-align: center; width: 100%;"><table style="width: 100%; marg
 
 splide.mount(); 
 			// Popup
-			$('body').append('<div id="pop-up-container" onclick="close_pop_up()"><div id="bg-black" class="fade-in"></div><div id="pop-up-wrapper" class="swirl-in-fwd gray-bg"><a href="#" target=""><img src="https://iili.io/HrjMQOQ.png" alt="tolejitu"/></a><button onclick="close_pop_up()">Klik di sini untuk tutup</button></div></div>');
+			$('body').append('<div id="pop-up-container" onclick="close_pop_up()"><div id="bg-black" class="fade-in"></div><div id="pop-up-wrapper" class="swirl-in-fwd gray-bg"><a href="#" target=""><img src="https://i.postimg.cc/ZRsbWn6c/POP-UP-TOLEJITU.png" alt="tolejitu"/></a><button onclick="close_pop_up()">Klik di sini untuk tutup</button></div></div>');
 
 			setTimeout(close_pop_up, 10000);
-
+	
 			document.getElementById('pop-up-wrapper').addEventListener('click', e => {
 				e.stopPropagation();
 			});
