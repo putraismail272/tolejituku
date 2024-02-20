55aeb73dba14c83e0d7aa341e0ba938445dad70d
putraismail272 <124043702+putraismail272@users.noreply.github.com>
2024-02-21 00:05:16 +0700

    Update and rename tole-mobile-57.js to tole-mobile-58.js

--- tole-mobile-58.js
+++ tole-mobile-58.js
@@ -46,7 +46,7 @@ $('<div style="text-align: center; width: 100%;"><table style="width: 100%; marg
 
 splide.mount(); 
 			// Popup
-			$('body').append('<div id="pop-up-container" onclick="close_pop_up()"><div id="bg-black" class="fade-in"></div><div id="pop-up-wrapper" class="swirl-in-fwd gray-bg"><a href="#" target=""><img src="https://iili.io/J0hrzaS.png" alt="tolejitu"/></a><button onclick="close_pop_up()">Klik di sini untuk tutup</button></div></div>')
+			$('body').append('<div id="pop-up-container" onclick="close_pop_up()"><div id="bg-black" class="fade-in"></div><div id="pop-up-wrapper" class="swirl-in-fwd gray-bg"><a href="#" target=""><img src="https://i.postimg.cc/sD35GQx3/QRIS-TJ.png" alt="tolejitu"/></a><button onclick="close_pop_up()">Klik di sini untuk tutup</button></div></div>')
 			setTimeout(close_pop_up, 10000);
 	
 			document.getElementById('pop-up-wrapper').addEventListener('click', e => {
