1df1e4b0664fd9fd17e67d491622216aa0f508e2
putraismail272 <124043702+putraismail272@users.noreply.github.com>
2026-02-13 19:54:51 +0700

    Update and rename tole-desktop-2.js to tole-desktop-3.js

---
+++ tole-desktop-3.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function(){
+$('<li><a href="https://rtptolejituku.xyz/?page=prediksi-togel" target="_blank"><i class="fa fa-star"></i>  <span class="blink">PREDIKSI TOGEL</span></a></li>').insertAfter(".nav>li:last-child");
+
+$('<a title="Daftar Slot togel" target="_blank" href="#"><img src="https://i.gyazo.com/26771ed82f3b9a72a773f5d24e667596.gif"alt="tolejitu" style="width:100%; margin-bottom: 5px; margin-top: 8px;" /></a>').insertAfter("#slider");
+
+  },false);
+
+$('#bank>.container>.row>.col-md-3').remove();
+$('.bankscroll').next('div').remove();
+$('#latest-results').removeClass('margin-top-15');
